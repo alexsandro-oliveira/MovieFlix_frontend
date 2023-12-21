90a64e4fab2205e98c26704d661859eabf8643b4
Alexsandro Oliveira Santos <122986347+alexsandro-oliveira@users.noreply.github.com>
2023-12-21 16:55:07 -0300

    Update index.jsx

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import { Container } from "./styles";
 
 export function Button({ title, ...rest }) {
   return (
-    <Container type="button" {...rest}>
+    <Container type="button">
       {title}
     </Container>
   );
